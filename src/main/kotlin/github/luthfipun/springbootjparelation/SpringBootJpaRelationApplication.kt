aaaf6610aa090928cfcd8b1cef056faa4334f35c
Luthfi Abdul Azis <luthfieabdulazis@gmail.com>
2022-09-17 18:23:41 +0700

    init

---
+++ src/main/kotlin/github/luthfipun/springbootjparelation/SpringBootJpaRelationApplication.kt
@@ -0,0 +1,11 @@
+package github.luthfipun.springbootjparelation
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootJpaRelationApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootJpaRelationApplication>(*args)
+}
